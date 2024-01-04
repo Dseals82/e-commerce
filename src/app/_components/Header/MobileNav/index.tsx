e3ac7db1217906f6cdcf909328ce6634255bef5c
Darius Seals <tristarmediagroupinc@gmail.com>
2024-01-04 12:33:48 -0500

    implemented the Header and MobilNav component and some minor styling

---
+++ src/app/_components/Header/MobileNav/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Header } from '../../../../payload/payload-types'
+
+const MobileNav = ({ header }: { header: Header }) => {
+  return <div>Mobile Nav</div>
+}
+
+export default MobileNav
